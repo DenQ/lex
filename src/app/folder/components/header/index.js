428b0725b1464cf1b987c48b967b4d8e50a85ecc
Denis Ivanov <denquick@gmail.com>
2020-12-30 21:44:26 +0300

    Done (#12)
    Closes #5

--- src/app/folder/components/header/index.js
+++ src/app/folder/components/header/index.js
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types';
 import { useHistory } from 'react-router-dom';
 import { Button } from '@material-ui/core';
 
+import { removeById } from 'api/folders';
+import { removeByFolderId } from 'api/words';
 import urlManager from 'common/utils/url-manager';
+
 import { controlNames } from '../../constants';
 
 
@@ -17,7 +20,17 @@ const Component = (props) => {
     };
 
     const toRemoveHandler = () => {
-        console.log('Removed', id);
+        removeById({ id })
+            .then((r) => {
+                return removeByFolderId({ folderId: id });
+            })
+            .then((r) => {
+                const to = urlManager.folders();
+                history.push(to);
+            })
+            .catch((e) => {
+                console.log('Catch error', e);
+            });
     };
 
     const collectionControls = [];
