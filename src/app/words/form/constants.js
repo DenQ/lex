8842e442687e9a6f974c7ff78ed309c7fa6390bb
denq <denquick@gmail.com>
2021-04-29 23:39:27 +0300

    check

--- src/app/words/form/constants.js
+++ src/app/words/form/constants.js
@@ -1,7 +1,7 @@
 // TODO: need use src/common/@types/words.js
 export const fieldNames = {
-		ID: 'id',
-	FOLDER_ID: 'folder_id',
+			ID: 'id',
+		FOLDER_ID: 'folder_id',
 	WORD_NATIVE: 'word_native',
 	WORD_TRANSLATION: 'word_translation',
 	NUMBER_OF_WINS: 'number_of_wins',
