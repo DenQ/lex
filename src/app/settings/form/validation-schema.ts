df31e8e4f15a4f142a9193996b2ba36998bccc26
denq <denquick@gmail.com>
2021-05-18 18:25:51 +0300

    Error messages

--- src/app/settings/form/validation-schema.ts
+++ src/app/settings/form/validation-schema.ts
@@ -1,8 +1,14 @@
 import * as Yup from 'yup';
-import { fieldNames } from './constants';
-
+import { errorMessages } from 'common/@messages/errors';
+import { fieldNames, initialValues } from './constants';
 
 export default Yup.object().shape({
-	[fieldNames.PLAY_COUNT_WORDS]: Yup.number().required('Is Required'),
-	[fieldNames.PLAY_MAX_COUNT_WINS]: Yup.number().required('Is Required'),
+	[fieldNames.PLAY_COUNT_WORDS]: Yup.number()
+		.required(errorMessages.IS_REQUIRED)
+		.positive(errorMessages.SHOULD_BE_POSITIVE)
+		.integer(errorMessages.SHOULD_BE_INTEGER),
+	[fieldNames.PLAY_MAX_COUNT_WINS]: Yup.number()
+		.required(errorMessages.IS_REQUIRED)
+		.positive(errorMessages.SHOULD_BE_POSITIVE)
+		.integer(errorMessages.SHOULD_BE_INTEGER),
 });
