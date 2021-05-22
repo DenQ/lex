7d168445947ae9fce12ff3a8bebfb8eae18c9976
denq <denquick@gmail.com>
2021-05-22 10:54:51 +0300

    hook

---
+++ src/lib/modals/confirmation/hook.tsx
@@ -0,0 +1,39 @@
+import { useCallback, useState } from 'react';
+
+export type OnConfirmationType = (params: { reset: () => void }) => void;
+
+interface Input {
+	onConfirmation: OnConfirmationType;
+}
+
+interface Output {
+	open: boolean;
+	toOpen: () => void;
+	onYes: () => void;
+	onCancel: () => void;
+}
+
+export const useConfirmationModal = ({ onConfirmation }: Input): Output => {
+	const [open, setOpen] = useState<boolean>(false);
+	const reset = useCallback(() => {
+		setOpen(false);
+	}, []);
+	const toOpen = useCallback(() => {
+		setOpen(true);
+	}, []);
+	const onYes = useCallback(() => {
+		onConfirmation({
+			reset,
+		});
+	}, [onConfirmation]);
+	const onCancel = useCallback(() => {
+		reset();
+	}, [reset]);
+
+	return {
+		open,
+		toOpen,
+		onYes,
+		onCancel,
+	};
+};
