2ccb2fc6f133e4b1a0dc625e4cda0826a1652eb9
denq <denquick@gmail.com>
2021-10-17 19:50:26 +0300

    find by key value

--- src/app/words/form/hooks/validate/index.tsx
+++ src/app/words/form/hooks/validate/index.tsx
@@ -1,7 +1,8 @@
 import { useCallback } from 'react';
 import { utils as validationUtils } from 'common/modules/validation';
+import findByKeyValue from 'common/utils/find-by-key-value';
+import { WordItemDTO } from 'common/@types/words';
 import validationWordSchema from '../../validationSchema';
-import findByKeyValue from '../../../../../common/utils/find-by-key-value';
 
 type Input = {
 	words: Array<any>;
@@ -22,8 +23,13 @@ type Output = {
 const useValidate = ({ words }: Input): Output => {
 	const validate = useCallback(
 		(values: Values): OutputValidateFn => {
+			// TODO: Need to extract to utils
 			const hasNotUnique = (key: string) => {
-				const targetEntity = findByKeyValue(words, key, values[key]);
+				const targetEntity = findByKeyValue<WordItemDTO>(
+					words,
+					key,
+					values[key]
+				);
 
 				return targetEntity && targetEntity.id !== values.id;
 			};
