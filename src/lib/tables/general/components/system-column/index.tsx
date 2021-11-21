0656f11723aa8e46e49f3f1b7b09576c1429cafd
denq <denquick@gmail.com>
2021-11-21 18:17:50 +0300

    reformat code

--- src/lib/tables/general/components/system-column/index.tsx
+++ src/lib/tables/general/components/system-column/index.tsx
@@ -2,21 +2,23 @@ import React from 'react';
 import { Button } from '@mui/material';
 
 type Props = {
-	row: any;
+  row: any;
 };
 
 const SystemColumn: React.FC<Props> = ({ row }) => {
-	const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
-		e.preventDefault();
-		e.stopPropagation();
-		alert(JSON.stringify(row, null, 2));
-	};
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    alert(JSON.stringify(row, null, 2));
+  };
 
-	return (
-		<>
-			<Button onClick={onClick} variant="text">Edit</Button>
-		</>
-	);
+  return (
+    <>
+      <Button onClick={onClick} variant="text">
+        Edit
+      </Button>
+    </>
+  );
 };
 
 export default React.memo(SystemColumn);
--- src/lib/tables/general/utsils/get-mock-rows/index.ts
+++ src/lib/tables/general/utsils/get-mock-rows/index.ts
@@ -1,17 +1,17 @@
 import faker from 'faker';
 
 function randomInteger(min: number, max: number): number {
-	return Math.floor(Math.random() * (max - min + 1)) + min;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 const getMockRows = (size: number): any =>
-	Array(size)
-		.fill(0)
-		.map((item, index) => ({
-			id: index + 1,
-			lastName: faker.name.lastName(),
-			firstName: faker.name.firstName(),
-			age: randomInteger(1, 100),
-		}));
+  Array(size)
+    .fill(0)
+    .map((item, index) => ({
+      id: index + 1,
+      lastName: faker.name.lastName(),
+      firstName: faker.name.firstName(),
+      age: randomInteger(1, 100),
+    }));
 
 export default getMockRows;
