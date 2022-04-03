237d8e41207415cd7025a87bdb7fa235d0f74539
denq <denquick@gmail.com>
2022-04-03 12:00:00 +0300

    Updates

---
+++ precache-manifest.bd951dab3b54c3ee087f062f4ee94e7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d79eea11228045ad0e490457fa38dd14",
+    "url": "/lex/index.html"
+  },
+  {
+    "revision": "9bbab37509555d36bd71",
+    "url": "/lex/static/css/main.13429945.chunk.css"
+  },
+  {
+    "revision": "0379221d4e2872965d98",
+    "url": "/lex/static/js/2.b245159c.chunk.js"
+  },
+  {
+    "revision": "5b5f97e47b49445205c92b16d3aa36e3",
+    "url": "/lex/static/js/2.b245159c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bbab37509555d36bd71",
+    "url": "/lex/static/js/main.26d617ad.chunk.js"
+  },
+  {
+    "revision": "8eebf7a0fc112fd1399d",
+    "url": "/lex/static/js/runtime-main.82796b04.js"
+  }
+]);
