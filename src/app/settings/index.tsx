35d25e6525cc04fd0b3064d1a2d49fc12bb4b33d
denq <denquick@gmail.com>
2022-04-08 21:37:21 +0300

    Settings page

--- src/app/settings/index.tsx
+++ src/app/settings/index.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import Grid from '@material-ui/core/Grid';
+import Box from '@mui/material/Box';
 import GeneralLayout from '../system/layout';
 import SettingsForm from './form';
-import { Header } from './components/header';
 import { buildBreadCrumbs } from './utils';
+import BreadCrumbs from '../../common/components/bread-crumbs';
+import Layout from '../folder/components/layout';
 
 type Props = {};
 
-const SettingsPage: React.FC<Props> = () => (
-	<GeneralLayout title="Settings">
-		<Header breadCrumbs={buildBreadCrumbs()} />
-		<Grid container justify="center" spacing={2}>
-			<SettingsForm />
-		</Grid>
-	</GeneralLayout>
-);
+const SettingsPage: React.FC<Props> = () => {
+  const breadcrumbs = buildBreadCrumbs();
+
+  return (
+    <GeneralLayout title="Settings">
+      <Layout>
+        <Box m={2}>
+          <BreadCrumbs data={breadcrumbs} />
+        </Box>
+        <Grid container spacing={2} justifyContent="center">
+          <Grid item>
+            <SettingsForm />
+          </Grid>
+        </Grid>
+      </Layout>
+    </GeneralLayout>
+  );
+};
 
 export default SettingsPage;
