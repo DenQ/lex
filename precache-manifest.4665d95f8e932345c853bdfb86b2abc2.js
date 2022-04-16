0c7348b06a69934a36c320080804057c8a8fc7d2
denq <denquick@gmail.com>
2022-04-16 19:58:10 +0300

    Updates

---
+++ precache-manifest.4665d95f8e932345c853bdfb86b2abc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85652926170bcfe27a0e68bf756a4254",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a5f15c314f796665603d",
+    "url": "./static/css/main.8663c929.chunk.css"
+  },
+  {
+    "revision": "9e44483b272f10a243dd",
+    "url": "./static/js/2.84d413f8.chunk.js"
+  },
+  {
+    "revision": "5b5f97e47b49445205c92b16d3aa36e3",
+    "url": "./static/js/2.84d413f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5f15c314f796665603d",
+    "url": "./static/js/main.101b08e8.chunk.js"
+  },
+  {
+    "revision": "7395a09a755e10d5f535",
+    "url": "./static/js/runtime-main.d6061c63.js"
+  }
+]);
