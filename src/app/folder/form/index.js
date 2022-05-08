3bfddf24194aa18b09ed3fc8148a41f307ceea9a
denq <denquick@gmail.com>
2022-05-08 17:17:59 +0300

    Improvement edit folder

--- src/app/folder/form/index.js
+++ src/app/folder/form/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { Form } from 'react-final-form';
 import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
+import Grid from '@mui/material/Grid';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { fieldNames, initialValues } from 'common/@types/folder';
@@ -13,81 +13,89 @@ import InputControl from 'app/words/form/components/input-control';
 import { submitHandler } from './utils';
 
 const useStyles = makeStyles({
-	id: {
-		display: 'none',
-	},
+  id: {
+    display: 'none',
+  },
 });
 
 const EntityForm = ({ initialValues, readOnly }) => {
-	const classes = useStyles();
-	const history = useHistory();
-	const validate = () => {};
+  const classes = useStyles();
+  const history = useHistory();
+  const validate = () => {};
 
-	const onSuccessSubmit = () => {
-		history.push(urlManager.folders());
-	};
+  const onSuccessSubmit = () => {
+    history.push(urlManager.folders());
+  };
 
-	return (
-		<Form
-			onSubmit={submitHandler({ onSuccessSubmit })}
-			validate={validate}
-			initialValues={initialValues}
-			render={({ handleSubmit, dirty }) => (
-				<form onSubmit={handleSubmit}>
-					<Grid container spacing={5}>
-						<Grid item>
-							<div className={classes.id}>
-								<InputControl
-									fieldName={fieldNames.ID}
-									placeholder="NEW"
-									readOnly={readOnly}
-									disabled
-								/>
-							</div>
-						</Grid>
+  return (
+    <Form
+      onSubmit={submitHandler({ onSuccessSubmit })}
+      validate={validate}
+      initialValues={initialValues}
+      render={({ handleSubmit, dirty }) => (
+        <form onSubmit={handleSubmit}>
+          <Grid
+            container
+            spacing={{
+              xs: 2,
+              md: 5,
+            }}
+          >
+            <Grid item>
+              <div className={classes.id}>
+                <InputControl
+                  fieldName={fieldNames.ID}
+                  placeholder="NEW"
+                  readOnly={readOnly}
+                  disabled
+                />
+              </div>
+            </Grid>
 
-						<Grid item xs={3}>
-							<InputControl
-								fieldName={fieldNames.NAME}
-								placeholder="Folder Name"
-								readOnly={readOnly}
-							/>
-						</Grid>
+            <Grid item xs={3}>
+              <InputControl
+                fieldName={fieldNames.NAME}
+                placeholder="Folder Name"
+                readOnly={readOnly}
+              />
+            </Grid>
 
-						<Grid item xs={3}>
-							<InputControl
-								fieldName={fieldNames.DESCRIPTION}
-								placeholder="Description"
-								readOnly={readOnly}
-							/>
-						</Grid>
+            <Grid item xs={3}>
+              <InputControl
+                fieldName={fieldNames.DESCRIPTION}
+                placeholder="Description"
+                readOnly={readOnly}
+              />
+            </Grid>
 
-						<Grid item>
-							{!readOnly && (
-								<Button type="submit" color="primary" disabled={!dirty}>
-									Save
-								</Button>
-							)}
-						</Grid>
-					</Grid>
-				</form>
-			)}
-		/>
-	);
+            <Grid item xs={2} md={1}>
+              {!readOnly && (
+                <Button type="submit" color="primary" disabled={!dirty}>
+                  Save
+                </Button>
+              )}
+            </Grid>
+
+            <Grid item xs={2} md={1} />
+          </Grid>
+        </form>
+      )}
+    />
+  );
 };
 
 EntityForm.propTypes = {
-	initialValues: PropTypes.shape({
-		id: PropTypes.number,
-		name: PropTypes.string,
-		description: PropTypes.string,
-	}),
-	readOnly: PropTypes.bool,
+  initialValues: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    description: PropTypes.string,
+  }),
+  readOnly: PropTypes.bool,
 };
 
 EntityForm.defaultProps = {
-	initialValues,
-	readOnly: false,
+  initialValues,
+  readOnly: false,
 };
 
 export default EntityForm;
--- src/app/words/form/index.js
+++ src/app/words/form/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Form } from 'react-final-form';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
+import Grid from '@mui/material/Grid';
 import Button from '@material-ui/core/Button';
 
 import RefreshContext from 'common/contexts/refetch-context';
@@ -13,113 +13,124 @@ import { submitHandler } from './utils';
 import InputControl from './components/input-control';
 
 const useStyles = makeStyles({
-	id: {
-		display: 'none',
-	},
+  id: {
+    display: 'none',
+  },
 });
 
 const EntityForm = ({ initialValues, readOnly, handleRemove, words }) => {
-	const classes = useStyles();
-	const { wordsReload, setWordsReload } = React.useContext(RefreshContext);
-	const { validate } = useValidate({ words });
+  const classes = useStyles();
+  const { wordsReload, setWordsReload } = React.useContext(RefreshContext);
+  const { validate } = useValidate({ words });
 
-	const onSuccessSubmit = ({ form }) => {
-		console.log('success', form);
-		setTimeout(() => {
-			form.restart(initialValues);
-			setWordsReload(wordsReload + 1);
-		}, 0);
-	};
-	const onClickRemove = () => {
-		const id = initialValues[fieldNames.ID];
-		handleRemove({ id });
-	};
-	/* eslint-disable react-hooks/exhaustive-deps */
-	const isCanRemove = React.useMemo(() => !!initialValues[fieldNames.ID], [
-		initialValues[fieldNames.ID],
-	]);
-	/* eslint-enable react-hooks/exhaustive-deps */
+  const onSuccessSubmit = ({ form }) => {
+    console.log('success', form);
+    setTimeout(() => {
+      form.restart(initialValues);
+      setWordsReload(wordsReload + 1);
+    }, 0);
+  };
+  const onClickRemove = () => {
+    const id = initialValues[fieldNames.ID];
 
-	return (
-		<Form
-			onSubmit={submitHandler({ onSuccessSubmit })}
-			validate={validate}
-			initialValues={initialValues}
-			render={({ handleSubmit, valid, dirty }) => (
-				<form onSubmit={handleSubmit}>
-					<Grid container spacing={5}>
-						<Grid item>
-							<div className={classes.id}>
-								<label>ID</label>
-								<InputControl
-									fieldName={fieldNames.ID}
-									placeholder="NEW"
-									readOnly
-									disabled
-									id="new"
-								/>
-							</div>
+    handleRemove({ id });
+  };
+  /* eslint-disable react-hooks/exhaustive-deps */
+  const isCanRemove = React.useMemo(
+    () => !!initialValues[fieldNames.ID],
+    [initialValues[fieldNames.ID]]
+  );
+  /* eslint-enable react-hooks/exhaustive-deps */
 
-							<div className={classes.id}>
-								<label>Folder ID</label>
-								<InputControl
-									fieldName={fieldNames.FOLDER_ID}
-									placeholder="NEW"
-									readOnly
-								/>
-							</div>
-						</Grid>
+  return (
+    <Form
+      onSubmit={submitHandler({ onSuccessSubmit })}
+      validate={validate}
+      initialValues={initialValues}
+      render={({ handleSubmit, valid, dirty }) => (
+        <form onSubmit={handleSubmit}>
+          <Grid
+            container
+            spacing={{
+              xs: 2,
+              md: 5,
+            }}
+            sx={{
+              margin: 1,
+            }}
+          >
+            <Grid item>
+              <div className={classes.id}>
+                <label>ID</label>
+                <InputControl
+                  fieldName={fieldNames.ID}
+                  placeholder="NEW"
+                  readOnly
+                  disabled
+                  id="new"
+                />
+              </div>
 
-						<Grid item xs={3}>
-							<InputControl
-								fieldName={fieldNames.WORD_NATIVE}
-								placeholder="Native"
-								readOnly={readOnly}
-							/>
-						</Grid>
+              <div className={classes.id}>
+                <label>Folder ID</label>
+                <InputControl
+                  fieldName={fieldNames.FOLDER_ID}
+                  placeholder="NEW"
+                  readOnly
+                />
+              </div>
+            </Grid>
 
-						<Grid item xs={3}>
-							<InputControl
-								fieldName={fieldNames.WORD_TRANSLATION}
-								placeholder="Translation"
-								readOnly={readOnly}
-							/>
-						</Grid>
+            <Grid item xs={3}>
+              <InputControl
+                fieldName={fieldNames.WORD_NATIVE}
+                placeholder="Native"
+                readOnly={readOnly}
+              />
+            </Grid>
 
-						<Grid item xs={1}>
-							<Button type="submit" color="primary" disabled={!dirty || !valid}>
-								Save
-							</Button>
-						</Grid>
+            <Grid item xs={3}>
+              <InputControl
+                fieldName={fieldNames.WORD_TRANSLATION}
+                placeholder="Translation"
+                readOnly={readOnly}
+              />
+            </Grid>
 
-						{isCanRemove && (
-							<Grid item xs={1}>
-								<Button type="button" color="secondary" onClick={onClickRemove}>
-									Remove
-								</Button>
-							</Grid>
-						)}
-					</Grid>
-				</form>
-			)}
-		/>
-	);
+            <Grid item xs={2} md={1}>
+              <Button type="submit" color="primary" disabled={!dirty || !valid}>
+                Save
+              </Button>
+            </Grid>
+
+            {isCanRemove && (
+              <Grid item xs={2} md={1}>
+                <Button type="button" color="secondary" onClick={onClickRemove}>
+                  Remove
+                </Button>
+              </Grid>
+            )}
+          </Grid>
+        </form>
+      )}
+    />
+  );
 };
 
 EntityForm.propTypes = {
-	initialValues: PropTypes.shape({
-		[fieldNames.ID]: PropTypes.number,
-		[fieldNames.FOLDER_ID]: PropTypes.number,
-		[fieldNames.WORD_NATIVE]: PropTypes.string,
-		[fieldNames.WORD_TRANSLATION]: PropTypes.string,
-	}),
-	readOnly: PropTypes.bool,
-	handleRemove: PropTypes.func,
+  initialValues: PropTypes.shape({
+    [fieldNames.ID]: PropTypes.number,
+    [fieldNames.FOLDER_ID]: PropTypes.number,
+    [fieldNames.WORD_NATIVE]: PropTypes.string,
+    [fieldNames.WORD_TRANSLATION]: PropTypes.string,
+  }),
+  readOnly: PropTypes.bool,
+  handleRemove: PropTypes.func,
 };
 
 EntityForm.defaultProps = {
-	initialValues,
-	readOnly: false,
+  initialValues,
+  readOnly: false,
 };
 
 export default EntityForm;
